f3574b283a3657e3ffd5d40dde7a762c1411fae8
Diogo-Castilho <diogo@castilho.me>
2018-04-23 16:30:30 +0100

    Some more stuff

--- NTRUEncrypt/Invert_Poly.py
+++ NTRUEncrypt/Invert_Poly.py
@@ -6,15 +6,23 @@ def invert_poly(poly, modulus):
     return poly
 
 
-def extendedGCD(poly_a, poly_b):
-    r_old = poly_a
-    r_new = poly_b
-    s_old = 1
-    s_new = 0
-    t_old = 0
-    t_new = 1
+def extendedGCD(poly_a: Polynomial, poly_b: Polynomial):
+    r = []
+    s = []
+    t = []
+    r.extend((poly_a, poly_b))
+    s.extend((1, 0))
+    t.extend((0, 1))
     
     end = [0] * poly_a.coefs.size
-    while r_new != end:
-        quotient = r_old.divide(r_new)
-        
+    round = 1
+    while [round] != end:
+        quotient = r[round].divide(r[round - 1])
+        r.append(r[round - 1] - quotient * r[round])
+        s.append(s[round - 1] - quotient * s[round])
+        t.append(t[round - 1] - quotient * t[round])
+        round += 1
+    result = []
+    result.extend((r[round - 1], s[round - 1], t[round - 1]))
+    return result
+
--- NTRUEncrypt/Polynomial.py
+++ NTRUEncrypt/Polynomial.py
@@ -1,37 +1,50 @@
 class Polynomial:
-
-    def __init__(self, coefs, N):
+    
+    def __init__(self, coefs):
         self.coefs = coefs
-        self.N
-
-
-
-    #def __mul__(self, other, modulus):
-    
-    
+        
     # Self coefficients and Other coefficients must have the same size
-    def multiplication(self, other):
-        size = self.coefs.size
+    def __mul__(self, other):
+        size = len(self.coefs)
         mult_coefs = size * [0]
-        for i in range(0, size):
-            for j in range(0, size):
-                mult_coefs[(i * j) % size] += (self.coefs[i] + other.coefs[j])
+        for i in range(size):
+            for j in range(size):
+                mult_coefs[(i + j) % size] += (self.coefs[i] * other.coefs[j])
         return Polynomial(mult_coefs)
     
-        
-    def coefs_mod(self, modulus):
-        for i in range(0, self.coefs.size):
-            self.coefs[i] = self.coefs[i] % modulus
-            
-    
-    def add(self, other, modulus): # FIXME
+    def __add__(self, other):
         add_coefs = []
-        for i in range(0, self.coefs.size):
+        for i in range(0, len(self.coefs)):
             add_coefs.append(self.coefs[i] + other.coefs[i])
         return Polynomial(add_coefs)
     
     def __cmp__(self, other):
-        for i in range(0, self.coefs.size):
+        for i in range(0, len(self.coefs)):
             if self.coefs[i] != other.coefs[i]:
                 return -1
         return 0
+    
+    def __sub__(self, other):
+        sub_coefs = []
+        for i in range (len(self.coefs)):
+            sub_coefs.append(self.coefs[i] - other.coefs[i])
+        return Polynomial(sub_coefs)
+
+    def coefs_mod(self, modulus):
+        for i in range(0, len(self.coefs)):
+            self.coefs[i] = self.coefs[i] % modulus
+
+    def degree(self):
+        for i in range(len(self.coefs), 0, -1):
+            if self.coefs[i] != 0:
+                return i
+
+    def division(self, other):
+        quotient = []
+        remainder = Polynomial(self.coefs[:])
+        end = [0] * self.coefs.size
+        while remainder != end and remainder.degree() >= other.degree():
+            temp = remainder[remainder.degree()] / other[other.degree()]
+            quotient += temp
+            remainder -= temp * other
+        return [quotient, remainder]
