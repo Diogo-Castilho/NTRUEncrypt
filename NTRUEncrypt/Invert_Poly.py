568a6ff42626eff408edb8f2497bcae08c97592f
Diogo-Castilho <diogo@castilho.me>
2018-04-26 02:16:02 +0100

    GCD almost corrected

--- NTRUEncrypt/Invert_Poly.py
+++ NTRUEncrypt/Invert_Poly.py
@@ -11,20 +11,31 @@ def extendedGCD(poly_a: Polynomial, poly_b: Polynomial):
     s = []
     t = []
     r.extend((poly_a, poly_b))
-    s.extend((1, 0))
-    t.extend((0, 1))
+    s.extend((Polynomial([1]), Polynomial([0])))
+    t.extend((Polynomial([0]), Polynomial([1])))
     
     end = [0] * len(poly_a.coefs)
     round = 1
     while [round] != end:
-        print(round)
-        print(r[round - 1].coefs)
+        print(r[round-1].coefs)
         print(r[round].coefs)
-        quotient = r[round] // (r[round - 1])
+        quotient = r[round - 1] // r[round]
+        print("Quotient")
         print(quotient.coefs)
-        r.append(r[round - 1] - quotient * r[round])
-        s.append(s[round - 1] - quotient * s[round])
-        t.append(t[round - 1] - quotient * t[round])
+        print("Reminder")
+        remainder = r[round - 1] - quotient * r[round]
+        remainder.reduce()
+        print(remainder.coefs)
+        r.append(remainder)
+        print(r[round + 1].coefs)
+        new_s = s[round - 1] - quotient * s[round]
+        new_s.reduce()
+        s.append(new_s)
+        print(s[round + 1].coefs)
+        new_t = t[round - 1] - quotient * t[round]
+        new_t.reduce()
+        t.append(new_t)
+        print(t[round + 1].coefs)
         round += 1
     result = []
     result.extend((r[round - 1], s[round - 1], t[round - 1]))
--- NTRUEncrypt/Polynomial.py
+++ NTRUEncrypt/Polynomial.py
@@ -1,3 +1,4 @@
+import time
 class Polynomial:
     
     def __init__(self, coefs):
@@ -21,13 +22,15 @@ class Polynomial:
     def __cmp__(self, other):
         for i in range(0, len(self.coefs)):
             if self.coefs[i] != other.coefs[i]:
-                return -1
-        return 0
+                return False
+        return True
     
     def __sub__(self, other):
-        sub_coefs = []
-        for i in range(len(self.coefs)):
-            sub_coefs.append(self.coefs[i] - other.coefs[i])
+        sub_coefs = self.coefs[:]
+        if len(sub_coefs) < len(other.coefs):
+            [sub_coefs.append(0) for i in range(0, len(other.coefs) - len(sub_coefs))]
+        for i in range(0, len(other.coefs)):
+            sub_coefs[i] = sub_coefs[i] - other.coefs[i]
         return Polynomial(sub_coefs)
 
     def coefs_mod(self, modulus):
@@ -38,18 +41,31 @@ class Polynomial:
         for i in range(len(self.coefs) - 1, -1, -1):
             if self.coefs[i] != 0:
                 return i
+        return 0
             
     def __floordiv__(self, other):
         quotient = Polynomial([0] * len(self.coefs))
         remainder = Polynomial(self.coefs[:])
-        end = Polynomial([0] * len(self.coefs))
+        end = Polynomial([0])
+        print(remainder.degree())
         while remainder != end and remainder.degree() >= other.degree():
+            print("remainder end")
+            print(remainder.coefs)
+            print(end.coefs)
+            print(remainder != end)
             coefs = [0] * len(self.coefs)
-            temp = remainder.coefs[remainder.degree()] // \
+            temp = remainder.coefs[remainder.degree()] / \
                    other.coefs[other.degree()]
+            print(temp)
             coefs[remainder.degree() - other.degree()] = temp
             poly = Polynomial(coefs)
             quotient += poly
             poly = poly * other
             remainder = remainder - poly
+            remainder.reduce()
+            time.sleep(1)
         return quotient
+    
+    def reduce(self):
+        degree = self.degree()
+        self.coefs = self.coefs[:degree + 1]
